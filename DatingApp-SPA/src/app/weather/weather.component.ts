adda01fafa8a6d7a10f23869278d0314ba94ace4
M-Atef91 <60261060+M-Atef91@users.noreply.github.com>
2020-03-23 01:26:16 +0200

    init

---
+++ DatingApp-SPA/src/app/weather/weather.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { error } from 'util';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent implements OnInit {
+  weather: any;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.GetWeather();
+  }
+  GetWeather() {
+    this.http.get('http://localhost:5000/WeatherForecast').subscribe(response => {
+      this.weather = response;
+    }, error => { 
+      console.log(error);
+    });
+  }
+}
---
+++ DatingApp.API/Models/WeatherForcastme.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DatingApp.API.Models
+{
+    public class WeatherForcastme
+    {
+        public DateTime Date { get; set; }
+        public int Temp { get; set; }
+        public string Summary { get; set; }
+        
+    }
+}
