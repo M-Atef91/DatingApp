cee7dafdc9822f114e5c83d37592921548d52211
M-Atef91 <60261060+M-Atef91@users.noreply.github.com>
2020-03-23 03:11:40 +0200

    adding migration and update db

---
+++ DatingApp.API/Data/Migrations/20200323010857_init.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherForcastme",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Temp = table.Column<int>(nullable: false),
+                    Summary = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherForcastme", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherForcastme");
+        }
+    }
+}
--- DatingApp.API/Models/WeatherForcastme.cs
+++ DatingApp.API/Models/WeatherForcastme.cs
@@ -4,6 +4,7 @@ namespace DatingApp.API.Models
 {
     public class WeatherForcastme
     {
+        public int id { get; set; }
         public DateTime Date { get; set; }
         public int Temp { get; set; }
         public string Summary { get; set; }
